897cd9c02bfdf48db6bd65150b2a7fef4bd13868
szywolak <szy.wolak@gmail.com>
2016-06-04 19:20:01 +0200

    00

---
+++ Projekt2-master/src/main/java/com/weather/UserController.java
@@ -0,0 +1,23 @@
+package com.weather;
+
+import org.apache.catalina.connector.Response;
+import org.omg.CORBA.portable.InputStream;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+
+
+
+}
